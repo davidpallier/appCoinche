5d1d5a20275517e894df33c69297cc9fd31600f8
davidpallier <david.pallier.etu@gmail.com>
2020-04-11 13:47:52 +0200

    Add files via upload

---
+++ serverSocketIO.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'TouPal' or request.form['password'] != 'OqhDGknZwD6Y':
+            error = 'Invalid username/password'
+        else:
+            return render_template('session.html')
+            
+    return render_template('login.html', error=error)
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: '+ str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
