96986ff7c2046f64c87f4053bcfaf0e42064f26e
davidpallier <david.pallier.etu@gmail.com>
2020-04-12 15:57:18 +0200

    Update basicClient.py

--- basicClient.py
+++ basicClient.py
@@ -1,12 +1,15 @@
 
 #Wait for server request
 If request is 'Status':
+ status = waitForUserToUpdateStatus()
+ sendToServer(request, status)
  
 Elif request is 'Play a card':
  card = waitForUserToPlayCard()
  sendToServer(request, card)
  #wait for server answer
  while answer is not valid:
+  cardNotValid()
   card = waitForUserToPlayCard()
   sendToServer(request, card)
-  wait for server answer
+  #wait for server answer
